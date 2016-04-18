308febb71302e7f77a8fbe439ea9f2155e898313
Cameron Banta <cbanta@gmail.com>
2016-04-18 12:13:24 -0500

    add index and value property

--- src/counter32.cc
+++ src/counter32.cc
@@ -20,7 +20,15 @@ NAN_MODULE_INIT(Counter32::Init) {
   tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
   Nan::SetPrototypeMethod(tpl, "plusOne", PlusOne);
+  Nan::SetPrototypeMethod(tpl, "plus", Plus);
   Nan::SetPrototypeMethod(tpl, "getValue", GetValue);
+  Nan::SetPrototypeMethod(tpl, "setValue", SetValue);
+
+  Nan::SetAccessor(tpl->InstanceTemplate(),
+    v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), "value"),
+    // v8::String::NewFromOneByte(v8::Isolate::GetCurrent(), (const uint8_t *)"value", v8::NewStringType::kNormal, 5),
+    ValueAccessorGet,
+    ValueAccessorSet);
 
   constructor.Reset(Nan::GetFunction(tpl).ToLocalChecked());
   Nan::Set(target, Nan::New("Counter32").ToLocalChecked(), Nan::GetFunction(tpl).ToLocalChecked());
@@ -46,11 +54,33 @@ NAN_METHOD(Counter32::PlusOne) {
   obj->value_ += 1;
 }
 
+NAN_METHOD(Counter32::Plus) {
+  Counter32* obj = Nan::ObjectWrap::Unwrap<Counter32>(info.This());
+  uint32_t value = info[0]->IsUndefined() ? 1 : (uint32_t)(Nan::To<double>(info[0]).FromJust());
+  obj->value_ += value;
+}
+
+NAN_METHOD(Counter32::SetValue) {
+  Counter32* obj = Nan::ObjectWrap::Unwrap<Counter32>(info.This());
+  uint32_t value = info[0]->IsUndefined() ? 1 : (uint32_t)(Nan::To<double>(info[0]).FromJust());
+  obj->value_ = value;
+}
+
 NAN_METHOD(Counter32::GetValue) {
   Counter32* obj = Nan::ObjectWrap::Unwrap<Counter32>(info.This());
   info.GetReturnValue().Set((double)obj->value_);
 }
 
+NAN_GETTER(Counter32::ValueAccessorGet){
+  Counter32* obj = Nan::ObjectWrap::Unwrap<Counter32>(info.This());
+  info.GetReturnValue().Set((double)obj->value_);
+}
+
+NAN_SETTER(Counter32::ValueAccessorSet){
+  Counter32* obj = Nan::ObjectWrap::Unwrap<Counter32>(info.This());
+  uint32_t value2 = value->IsUndefined() ? 0 : (uint32_t)(Nan::To<double>(value).FromJust());
+  obj->value_ = value2;
+}
 
 
 NAN_MODULE_INIT(InitAll) {
--- src/counter32.h
+++ src/counter32.h
@@ -16,7 +16,12 @@ class Counter32 : public Nan::ObjectWrap {
 
   static NAN_METHOD(New);
   static NAN_METHOD(PlusOne);
+  static NAN_METHOD(Plus);
+  static NAN_METHOD(SetValue);
   static NAN_METHOD(GetValue);
+
+  static NAN_GETTER(ValueAccessorGet);
+  static NAN_SETTER(ValueAccessorSet);
 
   static Nan::Persistent<v8::Function> constructor;
   uint32_t value_;
